b1aeed42baad6d08afdcf34487041c27d6d48097
punnch <punnchxd@gmail.com>
2026-02-05 20:53:21 +0300

    feat: add error vars + update interface

--- internal/todo/repository.go
+++ internal/todo/repository.go
@@ -2,13 +2,20 @@ package todo
 
 import (
 	"context"
-
-	"github.com/jackc/pgx/v5"
+	"errors"
 )
 
 // contract to service
 
 type TodoRepository interface {
-	Create(ctx context.Context, conn *pgx.Conn, task Task) (Task, error)
-	GetAll(ctx context.Context, conn *pgx.Conn) ([]Task, error)
+	Create(ctx context.Context, task Task) (Task, error)
+	GetAll(ctx context.Context) ([]Task, error)
+	Get(ctx context.Context, id int) (Task, error)
+	Delete(ctx context.Context, id int) error
+	Complete(ctx context.Context, id int) (Task, error)
 }
+
+var (
+	ErrNotFound     error = errors.New("task not found")
+	ErrInvalidTitle error = errors.New("invalid task title")
+)
